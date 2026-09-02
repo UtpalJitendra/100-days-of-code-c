3fab5edab3bf9dda7899d16382f9bcd2d1d656bf
UtpalJitendra <utpal.41777@stu.upes.ac.in>
2026-09-02 22:32:54 +0000

    Day 01

---
+++ Day01/q1.c
@@ -0,0 +1,22 @@
+/*
+ * Name : Utpal Jitendra
+ * Roll : 590041777
+ * Day : 01 Question: 1
+ * Date : 03-09-2026
+ *
+ * PROBLEM STATEMENT:
+ * Write a program to input two numbers and display their sum.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d\n", a + b);
+
+    return 0;
+}
