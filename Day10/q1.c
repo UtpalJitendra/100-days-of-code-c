5c90fed6dd523d6feeefc7bc018256af9b347454
UtpalJitendra <utpal.41777@stu.upes.ac.in>
2026-09-03 00:37:41 +0000

    Day 10

---
+++ Day10/q1.c
@@ -0,0 +1,34 @@
+/*
+ * Name : Utpal Jitendra
+ * Roll : 590041777
+ * Day : 10 Question: 1
+ * Date : 03-09-2026
+ *
+ * PROBLEM STATEMENT:
+ * Write a program to classify a triangle as Equilateral,
+ * Isosceles, or Scalene based on its side lengths.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+    {
+        printf("Equilateral\n");
+    }
+    else if (a == b || b == c || a == c)
+    {
+        printf("Isosceles\n");
+    }
+    else
+    {
+        printf("Scalene\n");
+    }
+
+    return 0;
+}
---
+++ Day10/q2.c
@@ -0,0 +1,57 @@
+/*
+ * Name : Utpal Jitendra
+ * Roll : 590041777
+ * Day : 10 Question: 2
+ * Date : 03-09-2026
+ *
+ * PROBLEM STATEMENT:
+ * Write a program to display the day of the week based
+ * on a number (1-7) using switch-case.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int day;
+
+    scanf("%d", &day);
+
+    switch (day)
+    {
+        case 1:
+            printf("Monday\n");
+            break;
+
+        case 2:
+            printf("Tuesday\n");
+            break;
+
+        case 3:
+            printf("Wednesday\n");
+            break;
+
+        case 4:
+            printf("Thursday\n");
+            break;
+
+        case 5:
+            printf("Friday\n");
+            break;
+
+        case 6:
+            printf("Saturday\n");
+            break;
+
+        case 7:
+            printf("Sunday\n");
+            break;
+
+        default:
+            printf("Invalid day\n");
+    }
+
+    return 0;
+}
+
+
