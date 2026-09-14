fd9c3e852678286145d51033940c1920a02c811c
UtpalJitendra <utpal.41777@stu.upes.ac.in>
2026-09-14 21:26:00 +0000

    Day23

---
+++ Day23/q2.c
@@ -0,0 +1,32 @@
+/*
+ * Name : Utpal Jitendra
+ * Roll : 590041777
+ * Day  : 23 Question: 44
+ *
+ * PROBLEM STATEMENT:
+ * Write a program to print the following pattern:
+ * *****
+ * *****
+ * *****
+ * *****
+ * *****
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
